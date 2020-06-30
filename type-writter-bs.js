3626e8f6f20f14b8af47b849e9ae31281370aec4
Thang Le <jakethecat21@gmail.com>
2020-06-30 19:26:37 +0700

    thêm background các page con và hiệu ứng

---
+++ type-writter-bs.js
@@ -0,0 +1,41 @@
+var aText = new Array(
+    "Đây là những sản phẩm bán chạy nhất...", 
+    "These are our best-selling products...", 
+    "Estos son nuestros productos más vendidos...", 
+    "Ce sont nos produits les plus vendus...", 
+    "这些是我们最畅销的产品...", 
+
+    );
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext-bs");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
