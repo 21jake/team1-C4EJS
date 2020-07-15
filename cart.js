737219fa05dc06ee89093101e30327b1d1fd0a3d
TungNguyenx99 <67421488+TungNguyenx99@users.noreply.github.com>
2020-07-16 00:26:10 +0700

    triển khai tính năng lên best seller và new arrival

--- cart.js
+++ cart.js
@@ -121,7 +121,7 @@ function addItemToCart(title, price, img) {
   <span class="cart-price cart-column">${price}</span>
   <div class="cart-quantity cart-column">
       <input class="cart-quantity-input" type="number" value="1">
-      <button class="btn btn-danger" type="button">Xóa</button>
+      <button class="btn btn-danger" type="button">Delete</button>
   </div>`
 
 
